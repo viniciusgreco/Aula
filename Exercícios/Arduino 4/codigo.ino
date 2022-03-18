232bbb92951d463425a5392255b8a070fcb880fb
viniciusgreco <79270068+viniciusgreco@users.noreply.github.com>
2022-03-18 00:27:27 -0300

    4
    .

---
+++ Exercícios/Arduino 4/codigo.ino
@@ -0,0 +1,21 @@
+const int BUTTON_PIN = 6; 
+const int LED_PIN1 = 3; 
+
+int buttonState = 0;
+void setup() {
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  buttonState = digitalRead(BUTTON_PIN);
+  if(buttonState == LOW) 
+  {
+    digitalWrite(LED_PIN1, HIGH);
+  }
+  else 
+
+  {
+    digitalWrite(LED_PIN1, LOW); 
+  }
+}
